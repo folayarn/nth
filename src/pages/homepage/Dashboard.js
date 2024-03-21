3479cad9eb7dbaaeadaf5386d6fd4a33b53e74d9
faisalabdulkadir <wizardfaisal@gmail.com>
2024-03-21 15:34:40 +0100

    minor refactoring

--- src/pages/homepage/Dashboard.js
+++ src/pages/homepage/Dashboard.js
@@ -3,89 +3,120 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import { BsArrowDownSquareFill, BsCalculatorFill } from "react-icons/bs";
-import { GrCurrency } from "react-icons/gr";
-import { FaTools } from "react-icons/fa";
 import Slide from "./Slide";
+import { toolsData, newsData } from "../../store/data";
+import { motion } from "framer-motion";
+import Button from "react-bootstrap/Button";
 
 function Dashboard() {
+  //function to handle shortening of text to display
+  const shortenText = (str, maxLen, separator = " ") => {
+    if (str.length <= maxLen) return str;
+    return str.substr(0, str.lastIndexOf(separator, maxLen)) + " ...";
+  };
+
+  const boxVariants = {
+    initial: {
+      scale: 1,
+    },
+    hover: {
+      scale: 1.07,
+      transition: {
+        duration: 15,
+        type: "spring",
+        bounce: 2,
+      },
+    },
+  };
+
   return (
     <Container fluid>
       <Row>
-        <Col>
+        <Col className="p-0 ">
           <Slide />
         </Col>
       </Row>
 
       <Row className="mt-5 g-4">
-        <h2 className="text-center fw-bold">Useful Tool</h2>
-        <Col>
-          <Card style={{border: "none"}}>
-            <Card.Body className="text-center">
-              <div className="d-flex justify-content-center mb-5">
-                <BsArrowDownSquareFill color="#3AC05E" size={50} />
-              </div>
-              <Card.Title className="fw-bold">Import Classification Tool</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card style={{border: "none"}}>
-            <Card.Body className="text-center">
-              <div className="d-flex justify-content-center mb-5">
-                <BsCalculatorFill color="#3AC05E" size={50} />
-              </div>
-              <Card.Title className="fw-bold">Duty Calculator</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card style={{border: "none"}}>
-            <Card.Body className="text-center">
-              <div className="d-flex justify-content-center mb-5">
-                <GrCurrency color="#3AC05E" size={50} />
-              </div>
-              <Card.Title className="fw-bold">Currency converter</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card style={{border: "none"}}>
-            <Card.Body className="text-center">
-              <div className="d-flex justify-content-center mb-5">
-                <FaTools color="#3AC05E" size={50} />
-              </div>
-              <Card.Title className="fw-bold">Customs Procedure Codes</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
+        <h2 className="fw-bold d-flex justify-content-center align-items-center">
+          <div className="triangle-left"></div>Latest News
+          <div className="triangle-right"></div>
+        </h2>
+        {newsData.map((news) => {
+          return (
+            <Col>
+              <motion.div
+                variants={boxVariants}
+                initial="initial"
+                whileHover="hover"
+              >
+                <Card
+                  style={{
+                    width: "18rem",
+                    padding: 0,
+                    border: "1px solid #3AC05E",
+                  }}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={news.newsImg}
+                    style={{
+                      width: "100%",
+                      height: "150px",
+                      objectFit: "cover",
+                      objectPosition: "center",
+                    }}
+                  />
+                  <Card.Body>
+                    <Card.Title className="fw-bold">{news.title}</Card.Title>
+                    <Card.Text>{shortenText(news.text, 40)}</Card.Text>
+                    <Button className="button-color">Read More</Button>
+                  </Card.Body>
+                </Card>
+              </motion.div>
+            </Col>
+          );
+        })}
+        <div className="d-flex justify-content-center">
+          <Button variant="outline" className="button-color-more" size="lg">
+            More
+          </Button>
+        </div>
       </Row>
 
-      <Row className="mt-5">
-        <h2 className="text-center fw-bold">Organizations</h2>
+      <Row
+        className="mt-5 g-4"
+        style={{ backgroundColor: "rgba(58, 192, 94, 0.1)" }}
+      >
+        <h2 className="d-flex justify-content-center align-items-center fw-bold">
+          <div className="triangle-left"></div>Useful Tool
+          <div className="triangle-right"></div>
+        </h2>
+
+        {toolsData.map((tool, index) => (
+          <Col key={index}>
+            <motion.div
+              whileHover={{ scale: 1.08 }}
+              whileTap={{ scale: 0.7 }}
+              transition={{
+                duration: 0.5,
+              }}
+            >
+              <Card
+                className="shadow rounded"
+                style={{ border: "1px solid #3AC05E", height: "100%" }}
+              >
+                <Card.Body className="text-center">
+                  <div className="d-flex justify-content-center mb-5">
+                    {tool.icon}
+                  </div>
+                  <Card.Title className="fw-bold">{tool.title}</Card.Title>
+                  <Card.Text>{tool.text}</Card.Text>
+                </Card.Body>
+              </Card>
+            </motion.div>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
--- src/pages/homepage/Slide.js
+++ src/pages/homepage/Slide.js
@@ -1,67 +1,29 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
-import calculator from "../../images/calculator.avif";
-import exportImg from "../../images/exportPic.avif";
-import importImg from "../../images/importPic.avif";
+import { slideData } from "../../store/data";
 
 export default function Slide() {
   return (
     <Carousel slide={false}>
-      <Carousel.Item>
-        <div className="d-block w-100 h-20 overflow-hidden">
-          <Image
-            src={calculator}
-            style={{
-              height: "300px",
-              width: "100vw",
-              objectFit: "cover",
-              objectPosition: "center",
-            }}
-            fluid
-          />
-        </div>
-        <Carousel.Caption>
-          <h2>Duty Calculator Tool</h2>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          src={importImg}
-          className="d-block w-100"
-          style={{
-            height: "300px",
-            width: "100vw",
-            objectFit: "cover",
-            objectPosition: "center",
-          }}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h2>Import Classification Tool</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image
-          src={exportImg}
-          className="d-block w-100"
-          style={{
-            height: "300px",
-            width: "100vw",
-            objectFit: "cover",
-            objectPosition: "center",
-          }}
-          alt="..."
-        />
-        <Carousel.Caption>
-          <h2>Export Classification Tool</h2>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {slideData.map((slide, index) => {
+        return (
+          <Carousel.Item key={index}>
+            <Image
+              src={slide.slideImg}
+              className="d-block w-100 object-fit-cover object-position-center vh-100"
+              alt={slide.alt}
+              fluid
+            />
+            <Carousel.Caption className="top-50">
+              <h1 className="bg-dark p-3 d-inline bg-opacity-50">
+                {slide.title}
+              </h1>
+              <p className="m-3">{slide.description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        );
+      })}
     </Carousel>
   );
 }
