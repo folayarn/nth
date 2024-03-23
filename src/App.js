659006c038a16b4d39c0d008a2ed8ec3ee30b6ce
faisalabdulkadir <wizardfaisal@gmail.com>
2024-03-23 20:50:00 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import './App.css';
-import './App.scss';
-import Layout from './Layout/Layout';
+import "./App.css";
+import "./App.scss";
+import Router from "./routes/Routes";
 
 function App() {
   return (
     <>
-     <Layout/> 
+      <Router />
     </>
   );
 }
