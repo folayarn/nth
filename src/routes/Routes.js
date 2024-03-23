6e096618db9c122e545935596a5bd1b92a92a9f5
faisalabdulkadir <wizardfaisal@gmail.com>
2024-03-23 15:41:18 +0100

    fixes to route

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,25 +4,24 @@ import Dashboard from "../pages/homepage/Dashboard";
 import About from "../pages/about/About";
 import Help from "../pages/help/Help";
 import News from "../pages/news/News";
+import Layout from "../Layout/Layout";
 import Usefultools from "../pages/usefultools/Usefultools";
 import Organisations from "../pages/organisations/Organisations";
 
 function Router() {
   return (
-    <>
-      <BrowserRouter>
-
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          <Route path="/Organisations" element={<Organisations/>} />
-          <Route path="/Usefultool" element={<Usefultools/>} />
-          <Route path="/News" element={<News />} />
-          <Route path="/Help" element={<Help />} />
-          <Route path="/About" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="Organisations" element={<Organisations />} />
+          <Route path="Usefultool" element={<Usefultools />} />
+          <Route path="News" element={<News />} />
+          <Route path="Help" element={<Help />} />
+          <Route path="About" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
